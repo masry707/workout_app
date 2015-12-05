25403ffe41249d475dae7ec7866039282745fe69
Ahmed El Masry <ahmed.elmasry707@gmail.com>
2015-12-05 15:25:44 +0200

    add validations to exercises

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,3 +1,9 @@
 class Exercise < ActiveRecord::Base
   belongs_to :user
+  validates :duration_in_min, presence: true
+  validates_numericality_of :duration_in_min, only_integer: true
+  validates :workout_details, presence: true
+  validates :workout_date, presence: true
+  validates :user_id, presence: true
+
 end
--- spec/features/creating_exercises_spec.rb
+++ spec/features/creating_exercises_spec.rb
@@ -22,4 +22,23 @@ RSpec.feature 'Creating exercises' do
     exercise = Exercise.last
     expect(page.current_path).to eq(user_exercise_path(@user, exercise))
   end
+
+  scenario 'with invalid inputs' do
+    visit '/'
+    click_link('My Lounge')
+    click_link 'New Workout'
+    expect(page).to have_link('Back')
+
+    fill_in 'Duration' , with: nil
+    fill_in 'Workout details', with: ''
+    fill_in 'Activity date', with: ''
+    click_button 'Create Exercise'
+
+    expect(page).to have_content('Exercise has not been created')
+    expect(page).to have_content('Duration in min can\'t be blank')
+    expect(page).to have_content('Duration in min is not a number')
+    expect(page).to have_content('Workout details can\'t be blank')
+    expect(page).to have_content('Workout date can\'t be blank')
+
+  end
 end
