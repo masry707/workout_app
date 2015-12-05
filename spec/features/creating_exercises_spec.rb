e8505df382136861c3e84bca42dffe5aad01747d
Ahmed El Masry <ahmed.elmasry707@gmail.com>
2015-12-05 14:43:41 +0200

    creating exercise spec

---
+++ spec/features/creating_exercises_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.feature 'Creating exercises' do
+  before do
+    @user = User.create!(email: 'user@example.com', password: 'password')
+    login_as(@user)
+  end
+
+  scenario 'with valid inputs' do
+    visit '/'
+
+    click_link 'My Lounge'
+    click_link 'New Workout'
+    expect(page).to have_link('Back')
+
+    fill_in 'Duration' , with: 70
+    fill_in 'Workout details', with: 'Weight Lifting'
+    fill_in 'Activity date', with: '2015-08-07'
+    click_button 'Create Exercise'
+
+    expect(page).to have_content('Exercise has been created')
+    exercise = Exercise.last
+    expect(page.current_path).to eq(user_exercise_path(@user, exercise))
+  end
+end
